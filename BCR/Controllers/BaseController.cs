5137bb57e91413fda89d7ccffacd7501f9ae07eb
liuxu <liuxu@zccninfo.com>
2018-12-13 14:01:41 +0800

    升级netcore2.0.9

--- BCR/Controllers/BaseController.cs
+++ BCR/Controllers/BaseController.cs
@@ -15,23 +15,12 @@ namespace BCR.Controllers
     {
         public static IHttpContextAccessor _accessor;
         public readonly IConfiguration _configuration;
-        //public static string uid = "";
-        //public static string username = "";
-        //public static int area =0;
         public static string connstr = "";
-        //public static bool isadmin = false;
         public BaseController(IHttpContextAccessor accessor, IConfiguration configuration)
         {
             _accessor = accessor;
             _configuration = configuration;
             connstr = configuration["connstr:mysql"];
-            //JwtUtils jwt = new JwtUtils();
-            //var token = _accessor.HttpContext.Request.Query["token"];
-            //var claim = jwt.DecodingJwt(token);
-            //uid = claim.uid;
-            //username = claim.username;
-            //area = claim.area;
-            //isadmin = claim.isadmin;
 
         }
        
